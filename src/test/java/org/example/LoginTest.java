cbe39dc6a5fb424fc4c8cb887a9640366801a2aa
Nikita Shults <hukutqaaaa1998@gmail.com>
2022-06-27 20:34:15 +0300

    TMS-1 Added LoginPage and tests

---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void userShouldLoginWithValidCredentials() {
+        loginPage.open();
+        loginPage.login("standard_user","secret_sauce");
+        assertTrue(driver.findElement(By.cssSelector(".title")).isDisplayed(), "User was not logged in");
+    }
+
+    @Test
+    public void passwordShouldBeRequiredForLogin() {
+        loginPage.login("standard_user","");
+        assertEquals(loginPage.getError(), "Epic sadface: Password is required", "The error is incorrect");
+
+    }
+
+
+}
